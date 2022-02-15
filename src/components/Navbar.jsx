346b7b72fc5e3656ab4bace10a1a1ced9b87d7cf
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-15 13:57:59 +0100

    finish navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { Search } from '@material-ui/icons'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
+import { Badge } from '@material-ui/core'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -38,11 +39,26 @@ const Input = styled.input`
 
 const Center = styled.div`
     flex: 1;
+    text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
 `;
 
 const Right = styled.div`
     flex: 1;
-`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+`;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`;
 
 const Navbar = () => {
   return (
@@ -52,11 +68,21 @@ const Navbar = () => {
                 <Language>EN</Language>
                 <SearchContainer>
                     <Input />
-                    <Search />
+                    <Search style={{color:"gray", fontsize:16}}/>
                 </SearchContainer>
             </Left>
-            <Center>center</Center>
-            <Right>right</Right>
+            <Center>
+                <Logo>Pearlicia Lux</Logo>
+            </Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined />
+                    </Badge>
+                </MenuItem>
+            </Right>
         </Wrapper>
     </Container>
   )

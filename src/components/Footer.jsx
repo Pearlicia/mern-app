2beefd8cf74bb2fe9edad61154aa78d6387bcd53
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-18 09:38:01 +0100

    finish footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Twitter, Pinterest } from "@material-ui/icons";
+import { Facebook, Instagram, Twitter, Pinterest, Phone, MailOutline, Room } from "@material-ui/icons";
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -17,24 +17,65 @@ const Logo = styled.h1`
 `;
 
 const Desc = styled.p`
-
+    margin: 20px 0px;
 `;
 
 const SocialContainer = styled.div`
-
+    display: flex;
 `;
 
 const SocialIcon = styled.div`
-
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
 `;
 
 const Center = styled.div`
     flex: 1;
+    padding: 20px;
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+    margin: 0px;
+    padding: 0px;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
 `;
 
 const Right = styled.div`
     flex: 1;
+    padding: 20px;
+
 `;
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+
+`;
+
+
 
 const Footer = () => {
   return (
@@ -46,23 +87,50 @@ const Footer = () => {
                 products for your home and office.
             </Desc>
             <SocialContainer>
-                <SocialIcon>
+                <SocialIcon color="3B5999">
                     <Facebook />    
                 </SocialIcon>
-                <SocialIcon>
+                <SocialIcon color="E4405F">
                     <Instagram />   
                 </SocialIcon>
-                <SocialIcon>
+                <SocialIcon color="55ACEE">
                     <Twitter />
                 </SocialIcon>
-                <SocialIcon>
+                <SocialIcon color="E60023">
                     <Pinterest />    
                 </SocialIcon>
             </SocialContainer>
             
         </Left>
-        <Center></Center>
-        <Right></Right>
+        <Center>
+            <Title>Useful Links</Title>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Men Fashion</ListItem>
+                <ListItem>Women Fashion</ListItem>
+                <ListItem>Unisex</ListItem>
+                <ListItem>Accessories</ListItem>
+                <ListItem>My Account</ListItem>
+                <ListItem>Order Tracking</ListItem>
+                <ListItem>WishList</ListItem>
+                <ListItem>Terms</ListItem>
+            </List>
+        </Center>
+        <Right>
+            <Title>Contact</Title>
+            <ContactItem>
+                <Room style={{marginRight:"10px"}}/> 876 Main St. Suite 500 Nigeria
+            </ContactItem>
+            <ContactItem>
+                <Phone style={{marginRight:"10px"}}/> +234 806-123-4565
+            </ContactItem>
+            <ContactItem>
+                <MailOutline style={{marginRight:"10px"}}/> contact@pearlicia-lux.com
+            </ContactItem>
+            <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+
+        </Right>
       
     </Container>
   )

d121dda7c67a1d707324d52622679b5f7867573c
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-19 22:00:42 +0100

    finish register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Home from "./pages/Home";
-import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Register />
     </div>
   );
 }

ed4e3912a6b9f78deb926cc1b7f04fe75a0e9725
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-20 17:04:20 +0100

    finish login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Home from "./pages/Home";
-import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div>
-      <Register />
+      <Login />
     </div>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,5 @@
 import styled from "styled-components"
 
-
-// url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") 
-
-
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -12,7 +8,8 @@ const Container = styled.div`
     rgba(255,255,255,0.5)
   ), 
     url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-    center;
+      center;
+    background-size: cover;
 
     display: flex;
     align-items: center;

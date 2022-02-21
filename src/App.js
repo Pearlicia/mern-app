e8af804efe67c150dcb6614668244b177b21e5d2
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-21 13:50:24 +0100

    finish cart page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Cart from "./pages/Cart";
 
 
+
 const App = () => {
   return (
     <div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
+import { Add, Remove } from '@material-ui/icons';
 
 const Container = styled.div`
 
@@ -52,10 +53,120 @@ const Info = styled.div`
     flex: 3;
 `;
 
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`;
+
+const Image = styled.img`
+    width: 200px;
+`;
+
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+const ProductName = styled.span`
+
+`;
+
+const ProductId = styled.span`
+
+`;
+
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+`;
+
+const ProductSize = styled.div`
+
+`;
+
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`;
+
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+`;
+
+
 const Summary = styled.div`
     flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
     
 `;
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+    
+`;
+
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props=>props.type === "total" && "500"};
+    font-size: ${props=>props.type === "total" && "24px"};
+
+    
+`;
+
+const SummaryItemText= styled.span`
+    
+    
+`;
+
+const SummaryItemPrice = styled.span`
+    
+    
+`;
+
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+    
+`;
+
 
 const Cart = () => {
   return (
@@ -66,6 +177,7 @@ const Cart = () => {
           <Title>YOUR BAG</Title>
           <Top>
               <TopButton>CONTINUE SHOPPING</TopButton>
+        
               <TopTexts>
                   <TopText>Shopping Bag(2)</TopText>
                   <TopText>Your Wishlist (0)</TopText>
@@ -76,12 +188,64 @@ const Cart = () => {
               <Info>
                   <Product>
                       <ProductDetail>
-                          <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/161418818-TD" />
+                          <Image src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?s=612x612" />
+                            <Details>
+                                <ProductName><b>Product:</b> JESSIE THUNDER SHOES</ProductName>
+                                <ProductId><b>ID:</b> 93813718293</ProductId>
+                                <ProductColor color="black"/>
+                                <ProductSize><b>Size:</b> 37.5</ProductSize>
+                            </Details>
                       </ProductDetail>
-                      <PriceDetail></PriceDetail>
+                      <PriceDetail>
+                          <ProductAmountContainer>
+                              <Add />
+                              <ProductAmount>2</ProductAmount>
+                              <Remove />
+                          </ProductAmountContainer>
+                          <ProductPrice>$ 30</ProductPrice>
+                      </PriceDetail>
+                  </Product>
+                  <Hr />
+                  <Product>
+                      <ProductDetail>
+                          <Image src="https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" />
+                            <Details>
+                                <ProductName><b>Product:</b> HAKURA T-SHIRT</ProductName>
+                                <ProductId><b>ID:</b> 93813718293</ProductId>
+                                <ProductColor color="gray"/>
+                                <ProductSize><b>Size:</b> Medium</ProductSize>
+                            </Details>
+                      </ProductDetail>
+                      <PriceDetail>
+                          <ProductAmountContainer>
+                              <Add />
+                              <ProductAmount>2</ProductAmount>
+                              <Remove />
+                          </ProductAmountContainer>
+                          <ProductPrice>$ 20</ProductPrice>
+                      </PriceDetail>
                   </Product>
               </Info>
-              <Summary>summary</Summary>
+              <Summary>
+                  <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                  <SummaryItem>
+                      <SummaryItemText>Subtotal</SummaryItemText>
+                      <SummaryItemPrice>$ 80</SummaryItemPrice>
+                  </SummaryItem>
+                  <SummaryItem>
+                      <SummaryItemText>Estimated Shipping</SummaryItemText>
+                      <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                  </SummaryItem>
+                  <SummaryItem>
+                      <SummaryItemText>Shipping Discount</SummaryItemText>
+                      <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                  </SummaryItem>
+                  <SummaryItem type="total">
+                      <SummaryItemText>Total</SummaryItemText>
+                      <SummaryItemPrice>$ 80</SummaryItemPrice>
+                  </SummaryItem>
+                  <Button>CHECKOUT NOW</Button>
+              </Summary>
           </Bottom>
       </Wrapper>
       <Footer />
